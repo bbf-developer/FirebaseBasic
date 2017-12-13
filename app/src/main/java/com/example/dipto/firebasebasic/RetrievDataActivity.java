17005f3d1bb3422f374213ed56927094484c080d
imtiazdipto <programmer.imtiaz@gmail.com>
2017-12-13 12:43:51 +0600

    ADD- retrieving data in listview

--- app/src/main/java/com/example/dipto/firebasebasic/RetrievDataActivity.java
+++ app/src/main/java/com/example/dipto/firebasebasic/RetrievDataActivity.java
@@ -2,20 +2,29 @@ package com.example.dipto.firebasebasic;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class RetrievDataActivity extends AppCompatActivity {
 
-    @BindView(R.id.showUserName)
-    TextView showUserName;
 
-    private FirebaseDatabase mFireBaseDataBase ;
-    private String mTextViewString ;
+    ArrayList<String> myList = new ArrayList<>();
+    @BindView(R.id.newListView)
+    ListView newListView;
+
+    private DatabaseReference mFireBaseDataBase;
+    private String mTextViewString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +32,38 @@ public class RetrievDataActivity extends AppCompatActivity {
         setContentView(R.layout.activity_retriev_data);
         ButterKnife.bind(this);
 
+        mFireBaseDataBase = FirebaseDatabase.getInstance().getReference();
+
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myList) ;
+        newListView.setAdapter(arrayAdapter);
+
+        mFireBaseDataBase.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String data = dataSnapshot.getValue(String.class);
+                myList.add(data) ;
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
